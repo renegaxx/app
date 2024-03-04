b62db719d7a020767492b03f70666c799ef32a29
Renanzin <renan.santos17senai@gmail.com>
2024-03-04 13:54:23 -0300

    isso ai
    é

---
+++ geradorToke/App.js
@@ -0,0 +1,63 @@
+import { View, Text, StyleSheet, Image  } from "react-native";
+
+
+
+export default function App() {
+  return (
+    <View>
+      <Text>
+        <Image source={require("../assets/logo.png")} style={ESTILO.logo}/>
+        Meu app!
+      </Text>
+    </View>
+  )
+}
+
+const ESTILO = StyleSheet.create({
+    container: {
+    flex: 1,
+    backgroundColor: "#f3f3ff",
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  logo: {
+    marginBottom: 60
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+
